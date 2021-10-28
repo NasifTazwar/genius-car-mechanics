f620dc0ba3274a621c76bf1fdd14d8e77a43a975
NasifTazwar <nasiftazwar@gmail.com>
2021-10-28 19:21:08 +0600

    1st

---
+++ src/Pages/AddService/AddService.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React from 'react';
+import { useForm } from "react-hook-form";
+import './AddService.css';
+
+
+const AddService = () => {
+    const { register, handleSubmit ,reset } = useForm();
+    
+    const onSubmit = data => {
+        console.log(data);
+
+        // axios.post('http://localhost:5000/services' , data)
+        axios.post('https://lit-cliffs-00085.herokuapp.com/services' , data)
+        .then(res=>{
+            console.log(res);
+            if(res.data.insertedId){
+                alert('Added data');
+                reset();
+            }
+        })
+    };
+    return (
+        <div className="add-service">
+            <h2>Add Service</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("name", { required: true, maxLength: 20 })} placeholder="Name" />
+                <textarea {...register("description")} placeholder="Description"/>
+                <input type="number" {...register("price")} placeholder="Price"/>
+                <input {...register("img")} placeholder="Image URL"/>
+                <input type="submit" />
+            </form>
+        </div>
+    );
+};
+
+export default AddService;
